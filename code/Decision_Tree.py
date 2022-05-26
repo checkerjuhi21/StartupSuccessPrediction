5697522c5742d3d4aa53fad4575c4a02f7af8e9b
Juhi Checker <juhichecker21@gmail.com>
2022-05-26 16:02:13 -0700

    final output

--- code/Decision_Tree.py
+++ code/Decision_Tree.py
@@ -12,17 +12,19 @@ from pyspark.ml.classification import LogisticRegression
 from pyspark.shell import spark
 from pyspark import SparkContext
 import csv
+import timeit
 
+start = timeit.default_timer()
 #Preprocessing Dataset
 dataset = spark.read.csv('startupdataset.csv', inferSchema=True, header=True)
 
 f_dataset = dataset.select(dataset.columns[9:])
 
-f_dataset.groupBy('acq/closed').count().show()
+# f_dataset.groupBy('acq/closed').count().show()
 
-f_dataset.printSchema()
-f_dataset.describe().show()
-f_dataset.columns
+# print("Schema: ",f_dataset.printSchema())
+# print("Final dataset: ",f_dataset.describe().show())
+# print("Final dataset columns: ",f_dataset.columns)
 
 assembler = VectorAssembler( inputCols= ['age_first_funding_year', 'age_last_funding_year', 'age_first_milestone_year', 'age_last_milestone_year', 'funding_rounds', 'funding_total_usd', 'milestones', 'is_CA', 'is_NY', 'is_MA', 'is_TX', 'is_otherstate', 'is_software', 'is_web', 'is_mobile', 'is_enterprise', 'is_advertising', 'is_gamesvideo', 'is_ecommerce', 'is_biotech', 'is_consulting', 'is_othercategory', 'has_VC', 'has_angel', 'has_roundA', 'has_roundB', 'has_roundC', 'has_roundD', 'avg_participants', 'is_top500'], outputCol="features")
 
@@ -30,19 +32,22 @@ assemb_output = assembler.transform(f_dataset)
 
 final_dataset = assemb_output.select('features', 'acq/closed')
 
-final_dataset.show()
+# print("Final Final dataset: ",final_dataset.show())
 
 #Training and Testing Split
 training_df, testing_df = final_dataset.randomSplit([0.7,0.3], 99)
 
-training_df.count()
-testing_df.count()
-final_dataset.count()
+# print("Training dataset count: ",training_df.count())
+training_count = training_df.count()
+# print("Testing dataset count: ",testing_df.count())
+testing_count = testing_df.count()
+# print("Final dataset count: ",final_dataset.count())
+finaldataset_count = final_dataset.count()
 
 #Predicting using Decision Tree
 dt_classifier = DecisionTreeClassifier(labelCol="acq/closed").fit(training_df)
 dt_predictions = dt_classifier.transform(testing_df)
-dt_predictions.show()
+# dt_predictions.show()
 
 impfeatu = dt_classifier.featureImportances
 
@@ -53,17 +58,17 @@ preds_and_labels = preds_and_labels.select(['prediction','label'])
 
 metrics = MulticlassMetrics(preds_and_labels.rdd.map(tuple))
 
-print("Confusion matrix: ", metrics.confusionMatrix().toArray())
+# print("Confusion matrix: ", metrics.confusionMatrix().toArray())
 
-print("Accuracy: ",metrics.accuracy)
+# print("Accuracy: ",metrics.accuracy)
 
-print("Precision 1 : ",metrics.precision(1.0))
+# print("Precision 1 : ",metrics.precision(1.0))
 
-print("Precision 0 : ",metrics.precision(0.0))
+# print("Precision 0 : ",metrics.precision(0.0))
 
-print("Recall 1 : ",metrics.recall(1.0))
+# print("Recall 1 : ",metrics.recall(1.0))
 
-print("Recall 0 : ",metrics.recall(0.0))
+# print("Recall 0 : ",metrics.recall(0.0))
 
 cm = metrics.confusionMatrix().toArray()
 accuracy = metrics.accuracy
@@ -72,8 +77,13 @@ precision0 = metrics.precision(0.0)
 recall1 = metrics.recall(1.0)
 recall0 = metrics.recall(0.0)
 
+end = timeit.default_timer()
+time = end-start
+
 values = { "Confusion Matrix":cm, "Accuracy" : accuracy, "Precision 1.0" : precision1,
-            "Precision 0.0": precision0, "Recall 1.0" : recall1, "Recall 0.0": recall0, "Important Feature": impfeatu }
+            "Precision 0.0": precision0, "Recall 1.0" : recall1, "Recall 0.0": recall0, 
+            "Important Feature": impfeatu,"Training count":training_count, "Testing count": testing_count, 
+            "Final Dataset count": finaldataset_count, "Time(s)": time}
 
 w = csv.writer(open("Result_DecisionTree.csv", "w"))
 
--- code/Logistic_Regression.py
+++ code/Logistic_Regression.py
@@ -11,17 +11,18 @@ from pyspark.sql.types import FloatType
 from pyspark.ml.classification import LogisticRegression
 from pyspark.shell import spark
 import csv
+import timeit
 
+start = timeit.default_timer()
 #Preprocessing Dataset
 dataset = spark.read.csv('startupdataset.csv', inferSchema=True, header=True)
 
 f_dataset = dataset.select(dataset.columns[9:])
 
-f_dataset.groupBy('acq/closed').count().show()
 
-f_dataset.printSchema()
-f_dataset.describe().show()
-f_dataset.columns
+# print("Schema: ",f_dataset.printSchema())
+# print("Final dataset: ",f_dataset.describe().show())
+# print("Final dataset columns: ",f_dataset.columns)
 
 assembler = VectorAssembler( inputCols= ['age_first_funding_year', 'age_last_funding_year', 'age_first_milestone_year', 'age_last_milestone_year', 'funding_rounds', 'funding_total_usd', 'milestones', 'is_CA', 'is_NY', 'is_MA', 'is_TX', 'is_otherstate', 'is_software', 'is_web', 'is_mobile', 'is_enterprise', 'is_advertising', 'is_gamesvideo', 'is_ecommerce', 'is_biotech', 'is_consulting', 'is_othercategory', 'has_VC', 'has_angel', 'has_roundA', 'has_roundB', 'has_roundC', 'has_roundD', 'avg_participants', 'is_top500'], outputCol="features")
 
@@ -29,25 +30,30 @@ assemb_output = assembler.transform(f_dataset)
 
 final_dataset = assemb_output.select('features', 'acq/closed')
 
-final_dataset.show()
+# print("Final Final dataset: ",final_dataset.show())
 
 #Training and Testing Split
 training_df, testing_df = final_dataset.randomSplit([0.7,0.3])
 
-training_df.count()
-testing_df.count()
-final_dataset.count()
+# print("Training dataset count: ",training_df.count())
+training_count = training_df.count()
+# print("Testing dataset count: ",testing_df.count())
+testing_count = testing_df.count()
+# print("Final dataset count: ",final_dataset.count())
+finaldataset_count = final_dataset.count()
 
 #Predicting using Logistic Regression
 log_classifier = LogisticRegression(maxIter=10, featuresCol="features", labelCol="acq/closed")
 
 model = log_classifier.fit(training_df)
 
-print(model.summary.areaUnderROC)
-
+# print(model.summary.areaUnderROC)
+ROC = model.summary.areaUnderROC
 lr_prediction = model.transform(testing_df)
 
-lr_prediction.show()
+# print("Prediction ouput: ")
+# lr_prediction.show()
+
 
 precision =  MulticlassClassificationEvaluator(labelCol="acq/closed", metricName = "weightedPrecision").evaluate(lr_prediction)
 
@@ -59,17 +65,17 @@ preds_and_labels = preds_and_labels.select(['prediction','label'])
 metrics = MulticlassMetrics(preds_and_labels.rdd.map(tuple))
 
 
-print("Confusion matrix: ", metrics.confusionMatrix().toArray())
+# print("Confusion matrix: ", metrics.confusionMatrix().toArray())
 
-print("Accuracy: ",metrics.accuracy)
+# print("Accuracy: ",metrics.accuracy)
 
-print("Precision 1 : ",metrics.precision(1.0))
+# print("Precision 1 : ",metrics.precision(1.0))
 
-print("Precision 0 : ",metrics.precision(0.0))
+# print("Precision 0 : ",metrics.precision(0.0))
 
-print("Recall 1 : ",metrics.recall(1.0))
+# print("Recall 1 : ",metrics.recall(1.0))
 
-print("Recall 0 : ",metrics.recall(0.0))
+# print("Recall 0 : ",metrics.recall(0.0))
 
 cm = metrics.confusionMatrix().toArray()
 accuracy = metrics.accuracy
@@ -78,12 +84,19 @@ precision0 = metrics.precision(0.0)
 recall1 = metrics.recall(1.0)
 recall0 = metrics.recall(0.0)
 
-values = { "Confusion Matrix":cm, "Accuracy" : accuracy, "Precision 1.0" : precision1,
-            "Precision 0.0": precision0, "Recall 1.0" : recall1, "Recall 0.0": recall0, "Important Feature": impfeatu }
+end = timeit.default_timer()
 
-w = csv.writer(open("Result_DecisionTree.csv", "w"))
+# print("Timing: ", end-start)
+# print("GroupBy: ",f_dataset.groupBy('acq/closed').count().show())
+time = end-start
+values = { "Confusion Matrix":cm, "Accuracy" : accuracy, "Precision 1.0" : precision1,
+            "Precision 0.0": precision0, "Recall 1.0" : recall1, "Recall 0.0": recall0, 
+            "Training count":training_count, "Testing count": testing_count, "Final Dataset count": finaldataset_count,
+            "ROC" : ROC, "Time(s)": time
+         }
+
+w = csv.writer(open("Result_LogisticRegression.csv", "w"))
 
 for key, val in values.items():
-
     # write every key and value to file
     w.writerow([key, val])
